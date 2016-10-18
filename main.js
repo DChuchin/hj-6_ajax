0dbbcc0a46b16fd764192674bb188d16751f0920
dmitry <dime10cents@gmail.com>
2016-10-18 22:53:57 +0600

    get json

--- main.js
+++ main.js
@@ -5,24 +5,28 @@ document.addEventListener('DOMContentLoaded', function() {
 			form = this.form,
 			body = 'email=' + encodeURIComponent(email.value) + '&password=' + encodeURIComponent(password.value);
 		console.log(body);
-		xhr.addEventListener('readystatechange', function() {
-			switch (xhr.readyState) {
-				case 1:
-					showPreloader();
-					break;
-				case 4:
-					hidePreloader();
-					break;
-			};
-
-		});
+		xhr.addEventListener('loadstart', loadStart);
+		xhr.addEventListener('loadend', loadEnd);
+		//xhr.addEventListener('load', showData);
+		xhr.addEventListener('error', showError);
+		// xhr.addEventListener('readystatechange', function() {
+		// 	switch (xhr.readyState) {
+		// 		case 1:
+		// 			showPreloader();
+		// 			break;
+		// 		case 4:
+		// 			hidePreloader();
+		// 			break;
+		// 	};
+		//
+		// });
 
 		xhr.open('POST', 'http://netology-hj-ajax.herokuapp.com/homework/login_json', true);
 		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 		xhr.send(body);
-		
-		
+
+
 
 		// showPreloader();
 
@@ -33,23 +37,54 @@ document.addEventListener('DOMContentLoaded', function() {
 		// 	console.log('good')
 		// 	console.log(data);
 		// });
-	
+
 		// xhr.addEventListener('error', function () {
 		// 	console.log('error');
 		// 	console.log(xhr.status +': ' + xhr.statusText);
 		// });
-			
+
 	});
 
 	function showPreloader() {
 		console.log('start');
-		preloader.style.display = "inline";
-		form.style.display = "none";
+		preloader.style.display = "inherit";
 	};
 
-	function hidePreloader() {
-		console.log('end');
+	function hidePreloader(e) {
 		preloader.style.display = "none";
+	};
+
+	function showData(data) {
+		user.style.display="inherit";
+		for (prop in data) {
+			console.log(prop + ': ' + data[prop]);
+		}
+	};
+	function showError(msg) {
+		hidePreloader();
+		showForm();
+		error.innerHTML = msg;
+	};
+
+	function loadStart() {
+		hideForm();
+		showPreloader();
+	};
+	function loadEnd() {
+		hidePreloader();
+		if (this.status == 200) {
+			var data = JSON.parse(this.responseText);
+			showData(data);
+		} else {
+			showForm();
+			showError(this.status + ': ' + this.statusText);
+		}
+	};
+	function showForm() {
+		form.style.display="inherit";
+	};
+	function hideForm() {
+		form.style.display="none";
 	};
 
 
